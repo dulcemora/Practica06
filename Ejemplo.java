56823beca3d86fe27c24e043b5e6e8d32d409420
dulcemora <dulcemora03@ciencias.unam.mx>
2021-11-16 01:28:19 -0600

    Agregando metodos

--- Ejemplo.java
+++ Ejemplo.java
@@ -11,8 +11,18 @@ public class Ejemplo {
 	}
 
 	sucesor(a);
-	System.out.println("El valor de a es: " + a);
+	System.out.println("El valor de a es: " + a); //Aqui se trata de paso de parametros por por valor ya que a no se esta modificando
 	sucesor(b);
-	System.out.println("El sucesor del lugar 14 es: " + b[b.length - 5]);
+	System.out.println("El sucesor del lugar 14 es: " + b[b.length - 5]); //Este es paso de parametros por referencia, es decir por medio de una especie de puntero
+	sucesor1(c);
+    }
 
+    public static void sucesor(int a) { //Metodo que nos da el sucesor de "a"
+	a = a + 1;
+    }
+
+    public static void sucesor1(int c) { //Metodo que da el sucesor de "c"
+	c = c + 1;
+	System.out.println("El sucesor de c es: " + c); //Por medio del s.o.p. el valor se modifica y al salir del metodo tendra un nuevo valor
+    }
 }
